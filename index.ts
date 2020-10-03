dc7fa2b943a0f27e87999ead1e5fb8ad534e303b
Huynh Khanh Nguyen <fealess95@gmail.com>
2020-10-03 17:30:39 +0700

    chore: export Loading

--- index.ts
+++ index.ts
@@ -3,5 +3,9 @@ export { default as Title } from './src/components/Title';
 export { default as Image } from './src/components/Image';
 export { default as Grid } from './src/components/Grid';
 export { default as Box } from './src/components/Box';
-export { default as Loading } from './src/components/Loading';
+export {
+  default as Loading,
+  BackdropVariant,
+  LoadingTypes,
+} from './src/components/Loading';
 export { default as cn } from './src/utils/classnames';
